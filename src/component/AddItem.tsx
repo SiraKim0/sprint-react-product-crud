87fd9a317679b86cfb98667281835d80f6f24e00
SiraKim0 <dosira686@naver.com>
2023-01-09 22:44:34 +0900

    fix: add form validation

--- src/component/AddItem.tsx
+++ src/component/AddItem.tsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 import { Item } from "./Item.type";
 import "./ItemForm.style.css";
-import { InputLabel, MenuItem, TextField, FormControl } from "@mui/material";
+import {
+  InputLabel,
+  MenuItem,
+  TextField,
+  FormControl,
+  Button,
+} from "@mui/material";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 type Props = {
@@ -10,10 +16,18 @@ type Props = {
 };
 
 const AddItem = (props: Props) => {
-  const [category, setCategory] = useState("");
-  const [brand, setBrand] = useState("");
-  const [itemName, setItemName] = useState("");
-  const [price, setPrice] = useState("");
+  const { onBackBtnClickHnd, onSubmitClickHnd } = props;
+
+  const [itemData, setItemData] = useState({
+    category: "",
+    brand: "",
+    itemName: "",
+    price: "",
+  });
+  const [itemNameMsg, setItemNameMsg] = useState("");
+  const [priceMsg, setPriceMsg] = useState("");
+  const [isItemName, setIsItemName] = useState(false);
+  const [isPrice, setIsPrice] = useState(false);
 
   const categorySelect = [
     "반지",
@@ -34,47 +48,69 @@ const AddItem = (props: Props) => {
     "스테이잼",
   ];
 
-  const { onBackBtnClickHnd, onSubmitClickHnd } = props;
-
   const onCategoryChangeHnd = (e: SelectChangeEvent) => {
-    setCategory(e.target.value);
+    setItemData({ ...itemData, category: e.target.value });
   };
   const onBrandChangeHnd = (e: SelectChangeEvent) => {
-    setBrand(e.target.value);
+    setItemData({ ...itemData, brand: e.target.value });
   };
   const onItemNameChangeHnd = (e: any) => {
-    setItemName(e.target.value);
+    setItemData({ ...itemData, itemName: e.target.value });
+    if (e.target.value.length < 3 || e.target.value.length > 20) {
+      setItemNameMsg("상품명은 3에서 20 글자 사이로 입력하세요.");
+      setIsItemName(false);
+    } else {
+      setItemNameMsg("");
+      setIsItemName(true);
+    }
   };
   const onPriceChangeHnd = (e: any) => {
-    setPrice(e.target.value);
+    const onlyNumber = e.target.value.replace(/[^0-9]/g, "");
+    setItemData({ ...itemData, price: onlyNumber });
+    if (e.target.value < 1000) {
+      setPriceMsg("입력한 금액이 적습니다. 1000원 이상부터 입력하세요.");
+      setIsPrice(false);
+    } else {
+      setPriceMsg("");
+      setIsPrice(true);
+    }
   };
 
   const onSubmitBtnClickHnd = (e: any) => {
     e.preventDefault();
-    const data: Item = {
-      id: new Date().toJSON.toString(),
-      category: category,
-      brand: brand,
-      itemName: itemName,
-      price: price,
-    };
-    onSubmitClickHnd(data);
-    onBackBtnClickHnd();
+    if (itemData.category === "") {
+      alert("카테고리 선택하세요.");
+    } else if (itemData.brand === "") {
+      alert("브랜드를 선택하세요.");
+    } else if (!isItemName) {
+      alert("상품명을 입력하세요.");
+    } else if (!isPrice) {
+      alert("가격을 입력하세요");
+    } else {
+      const data: Item = {
+        id: new Date().toJSON().toString(),
+        category: itemData.category,
+        brand: itemData.brand,
+        itemName: itemData.itemName,
+        price: itemData.price,
+      };
+      onSubmitClickHnd(data);
+      onBackBtnClickHnd();
+    }
   };
 
   return (
     <div className="form-container">
-      <div>
-        <h3>상품 등록</h3>
-      </div>
+      <h2 className="list-header">상품 등록</h2>
       <form onSubmit={onSubmitBtnClickHnd}>
         <FormControl sx={{ m: 2, minWidth: 150 }}>
           <InputLabel id="demo-select-small">카테고리</InputLabel>
           <Select
             labelId="demo-select-small"
             id="demo-select-small"
-            value={category}
+            value={itemData.category}
             label="카테고리"
+            name="category"
             onChange={onCategoryChangeHnd}
           >
             {categorySelect.map((item, idx) => {
@@ -91,8 +127,9 @@ const AddItem = (props: Props) => {
           <Select
             labelId="demo-select-small"
             id="demo-select-small"
-            value={brand}
+            value={itemData.brand}
             label="브랜드"
+            name="brand"
             onChange={onBrandChangeHnd}
           >
             {brandSelect.map((item, idx) => {
@@ -105,18 +142,39 @@ const AddItem = (props: Props) => {
           </Select>
         </FormControl>
         <TextField
+          sx={{ m: 2, minWidth: 150 }}
           label="상품명"
           variant="outlined"
+          value={itemData.itemName}
+          name="itemName"
+          autoComplete="off"
+          placeholder="상품명을 입력하세요."
           onChange={onItemNameChangeHnd}
         />
+        <span className="error-message">{itemNameMsg}</span>
         <TextField
+          sx={{ m: 2, minWidth: 150 }}
           label="가격"
           variant="outlined"
+          value={itemData.price}
+          name="price"
+          autoComplete="off"
+          placeholder="가격을 입력하세요. 숫자만 가능합니다."
           onChange={onPriceChangeHnd}
         />
-        <div>
-          <input type="button" value="Back" onClick={onBackBtnClickHnd} />
-          <input type="submit" value="Add Item" />
+        <span className="error-message">{priceMsg}</span>
+        <div className="action-btn">
+          <Button
+            variant="outlined"
+            type="button"
+            value="Back"
+            onClick={onBackBtnClickHnd}
+          >
+            뒤로가기
+          </Button>
+          <Button variant="contained" type="submit">
+            상품 등록
+          </Button>
         </div>
       </form>
     </div>
--- src/component/EditItem.tsx
+++ src/component/EditItem.tsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import { Item } from "./Item.type";
 import "./ItemForm.style.css";
+import {
+  InputLabel,
+  MenuItem,
+  TextField,
+  FormControl,
+  Button,
+} from "@mui/material";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 type Props = {
   data: Item;
@@ -11,62 +19,167 @@ type Props = {
 const EditItem = (props: Props) => {
   const { data, onBackBtnClickHnd, onUpdateClickHnd } = props;
 
-  const [category, setCategory] = useState(data.category);
-  const [brand, setBrand] = useState(data.brand);
-  const [itemName, setItemName] = useState(data.itemName);
-  const [price, setPrice] = useState(data.price);
+  // const [category, setCategory] = useState(data.category);
+  // const [brand, setBrand] = useState(data.brand);
+  // const [itemName, setItemName] = useState(data.itemName);
+  // const [price, setPrice] = useState(data.price);
+  const [itemData, setItemData] = useState({
+    category: data.category,
+    brand: data.brand,
+    itemName: data.itemName,
+    price: data.price,
+  });
+  const [itemNameMsg, setItemNameMsg] = useState("");
+  const [priceMsg, setPriceMsg] = useState("");
+  const [isItemName, setIsItemName] = useState(true);
+  const [isPrice, setIsPrice] = useState(true);
 
-  const onCategoryChangeHnd = (e: any) => {
-    setCategory(e.target.value);
+  const categorySelect = [
+    "반지",
+    "목걸이",
+    "귀걸이",
+    "이어커프",
+    "팔찌",
+    "발찌",
+  ];
+  const brandSelect = [
+    "위아몬즈",
+    "a14",
+    "마마카사르",
+    "룬느",
+    "프릿",
+    "멕코이",
+    "엠엠이",
+    "스테이잼",
+  ];
+
+  const onCategoryChangeHnd = (e: SelectChangeEvent) => {
+    setItemData({ ...itemData, category: e.target.value });
   };
-  const onBrandChangeHnd = (e: any) => {
-    setBrand(e.target.value);
+  const onBrandChangeHnd = (e: SelectChangeEvent) => {
+    setItemData({ ...itemData, brand: e.target.value });
   };
   const onItemNameChangeHnd = (e: any) => {
-    setItemName(e.target.value);
+    setItemData({ ...itemData, itemName: e.target.value.trim() });
+    if (e.target.value.length < 3 || e.target.value.length > 20) {
+      setItemNameMsg("상품명은 3에서 20 글자 사이로 입력하세요.");
+      setIsItemName(false);
+    } else {
+      setItemNameMsg("");
+      setIsItemName(true);
+    }
   };
   const onPriceChangeHnd = (e: any) => {
-    setPrice(e.target.value);
+    const onlyNumber = e.target.value.replace(/[^0-9]/g, "");
+    setItemData({ ...itemData, price: onlyNumber });
+    if (e.target.value < 1000) {
+      setPriceMsg("입력한 금액이 적습니다. 1000원 이상부터 입력하세요.");
+      setIsPrice(false);
+    } else {
+      setPriceMsg("");
+      setIsPrice(true);
+    }
   };
 
   const onSubmitBtnClickHnd = (e: any) => {
     e.preventDefault();
-    const updatedData: Item = {
-      id: data.id,
-      category: category,
-      brand: brand,
-      itemName: itemName,
-      price: price,
-    };
-    onUpdateClickHnd(updatedData);
-    onBackBtnClickHnd();
+    if (itemData.category === "") {
+      alert("카테고리 선택하세요.");
+    } else if (itemData.brand === "") {
+      alert("브랜드를 선택하세요.");
+    } else if (!isItemName) {
+      alert("상품명을 입력하세요.");
+    } else if (!isPrice) {
+      alert("가격을 입력하세요");
+    } else {
+      const updatedData: Item = {
+        id: data.id,
+        category: itemData.category,
+        brand: itemData.brand,
+        itemName: itemData.itemName,
+        price: itemData.price,
+      };
+      onUpdateClickHnd(updatedData);
+      onBackBtnClickHnd();
+    }
   };
 
   return (
     <div className="form-container">
-      <div>
-        <h3>Edit Item Form</h3>
-      </div>
+      <h2 className="list-header">상품 등록</h2>
       <form onSubmit={onSubmitBtnClickHnd}>
-        <div>
-          <label>분류: </label>
-          <input type="text" value={category} onChange={onCategoryChangeHnd} />
-        </div>
-        <div>
-          <label>브랜드: </label>
-          <input type="text" value={brand} onChange={onBrandChangeHnd} />
-        </div>
-        <div>
-          <label>상품명: </label>
-          <input type="text" value={itemName} onChange={onItemNameChangeHnd} />
-        </div>
-        <div>
-          <label>상품가격: </label>
-          <input type="text" value={price} onChange={onPriceChangeHnd} />
-        </div>
-        <div>
-          <input type="button" value="Back" onClick={onBackBtnClickHnd} />
-          <input type="submit" value="Update Item" />
+        <FormControl sx={{ m: 2, minWidth: 150 }}>
+          <InputLabel id="demo-select-small">카테고리</InputLabel>
+          <Select
+            labelId="demo-select-small"
+            id="demo-select-small"
+            value={itemData.category}
+            label="카테고리"
+            name="category"
+            onChange={onCategoryChangeHnd}
+          >
+            {categorySelect.map((item, idx) => {
+              return (
+                <MenuItem key={idx} value={item}>
+                  {item}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+        <FormControl sx={{ m: 2, minWidth: 150 }}>
+          <InputLabel id="demo-select-small">브랜드</InputLabel>
+          <Select
+            labelId="demo-select-small"
+            id="demo-select-small"
+            value={itemData.brand}
+            label="브랜드"
+            name="brand"
+            onChange={onBrandChangeHnd}
+          >
+            {brandSelect.map((item, idx) => {
+              return (
+                <MenuItem key={idx} value={item}>
+                  {item}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+        <TextField
+          sx={{ m: 2, minWidth: 150 }}
+          label="상품명"
+          variant="outlined"
+          value={itemData.itemName}
+          name="itemName"
+          autoComplete="off"
+          placeholder="상품명을 입력하세요."
+          onChange={onItemNameChangeHnd}
+        />
+        <span className="error-message">{itemNameMsg}</span>
+        <TextField
+          sx={{ m: 2, minWidth: 150 }}
+          label="가격"
+          variant="outlined"
+          value={itemData.price}
+          name="price"
+          autoComplete="off"
+          placeholder="가격을 입력하세요. 숫자만 가능합니다."
+          onChange={onPriceChangeHnd}
+        />
+        <span className="error-message">{priceMsg}</span>
+        <div className="action-btn">
+          <Button
+            variant="outlined"
+            type="button"
+            value="Back"
+            onClick={onBackBtnClickHnd}
+          >
+            뒤로가기
+          </Button>
+          <Button variant="contained" type="submit">
+            상품 등록
+          </Button>
         </div>
       </form>
     </div>
